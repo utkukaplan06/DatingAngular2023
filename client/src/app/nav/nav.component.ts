d6fe2977c7dd0d7c1aeeb46d1d70d955a17fdd28
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2023-10-16 17:09:17 +0300

    Bölüm 5 Video 62

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -10,14 +13,21 @@ export class NavComponent implements OnInit {
 
   model: any = {}
 
-  constructor() {}
+  constructor(public accountService: AccountService) {}
 
   ngOnInit(): void {
-
   }
   
   login(){
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => console.log(error)
+    })
   }
 
+  logout(){
+    this.accountService.logout();
+  }
 }
