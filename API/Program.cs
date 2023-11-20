e19632e2f447acbf448bff443c3d18f4e0f400d5
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2023-11-20 10:58:16 +0300

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,15 +19,20 @@ builder.Services.AddDbContext<DataContext>(opt =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+
 //builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+   .WithOrigins("https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     // app.UseSwagger();
     // app.UseSwaggerUI();
 }
