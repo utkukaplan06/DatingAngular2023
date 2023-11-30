d373a375a839e6633dd25816d52d936e0094421a
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2023-11-30 14:03:33 +0300

    Before 91

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,8 @@
 using API.Data;
+using API.Data.Migrations;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -43,5 +43,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope  = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
